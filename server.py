f440095598793e68111fcd18a7d0924e6b10925f
BugHunter34 <adam.tomala@seznam.cz>
2026-04-02 16:00:11 +0000

    MongoDB

--- server.py
+++ server.py
@@ -1,10 +1,28 @@
+import datetime
+from datetime import datetime
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import getpass
 import uvicorn
+import os
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
 
+# --- config ---
 app = FastAPI()
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://Admin:<secret>@chatdb.llkjj5f.mongodb.net/") 
+client = MongoClient(MONGO_URI)
+
+# --- MongoDB Connection ---
+try:
+    client.admin.command('ping')
+    print("Successfully connected to MongoDB!")
+    db = client["chat_database"]
+    users_collection = db["users"]
+except ConnectionFailure:
+    print("Failed to connect to MongoDB.")
+    
 
 # middleware to allow CORS
 app.add_middleware(
@@ -25,10 +43,13 @@ def login(request_data: LoginRequest):
     print(f"Ping received from client user: {request_data.name}")
     # username for Hello
     server_user = getpass.getuser() 
+    # MongoDB insert
+    users_collection.insert_one({"name": request_data.name, "timestamp": datetime.utcnow()})
+
     return {
         "status": "success",
         "server_user": server_user
     }
-
+    
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
